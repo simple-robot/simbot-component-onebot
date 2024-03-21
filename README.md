d2191e66800cf2cffc55a3f453ff1d74ace68d74
ForteScarlet <ForteScarlet@163.com>
2024-03-21 17:25:39 +0800

    INIT

---
+++ README.md
@@ -0,0 +1,23 @@
+<!--suppress HtmlDeprecatedAttribute -->
+<div align="center">
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset=".simbot/logo-dark.svg">
+  <source media="(prefers-color-scheme: light)" srcset=".simbot/logo.svg">
+  <img alt="simbot logo" src=".simbot/logo.svg" width="260" />
+</picture>
+<h2>
+    ~ Simple Robot ~ <br/> <small>OneBot Component</small>
+</h2>
+<a href="https://github.com/simple-robot/simbot-component-onebot/releases/latest"><img alt="release" src="https://img.shields.io/github/v/release/simple-robot/simbot-component-onebot" /></a>
+<a href="https://repo1.maven.org/maven2/love/forte/simbot/component/simbot-component-onebot-api/" target="_blank">
+  <img alt="release" src="https://img.shields.io/maven-central/v/love.forte.simbot.component/simbot-component-onebot-api" /></a>
+   <hr>
+   <img alt="stars" src="https://img.shields.io/github/stars/simple-robot/simbot-component-onebot" />
+   <img alt="forks" src="https://img.shields.io/github/forks/simple-robot/simbot-component-onebot" />
+   <img alt="watchers" src="https://img.shields.io/github/watchers/simple-robot/simbot-component-onebot" />
+   <img alt="repo size" src="https://img.shields.io/github/repo-size/simple-robot/simbot-component-onebot" />
+   <img alt="issues" src="https://img.shields.io/github/issues-closed/simple-robot/simbot-component-onebot?color=green" />
+   <img alt="last commit" src="https://img.shields.io/github/last-commit/simple-robot/simbot-component-onebot" />
+   <a href="./COPYING"><img alt="copying" src="https://img.shields.io/github/license/simple-robot/simbot-component-onebot" /></a>
+
+</div>
---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024. ForteScarlet.
+ *
+ * This file is part of simbot-component-telegram.
+ *
+ * simbot-component-telegram is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * simbot-component-telegram is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with simbot-component-telegram.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    mavenLocal()
+}
+
+val kotlinVersion: String = libs.versions.kotlin.get()
+
+dependencies {
+    implementation(kotlin("gradle-plugin", kotlinVersion))
+    implementation(kotlin("serialization", kotlinVersion))
+    implementation(libs.bundles.dokka)
+
+    // see https://github.com/gradle-nexus/publish-plugin
+    implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
+
+    // simbot suspend transform gradle common
+    implementation(libs.simbot.gradle)
+
+    // suspend transform
+    implementation(libs.suspend.transform.gradle)
+
+    // gradle common
+    implementation(libs.bundles.gradle.common)
+}
+
+//tasks.withType<KotlinCompile> {
+//    kotlinOptions {
+//        languageVersion = "2.0"
+//    }
+//}
