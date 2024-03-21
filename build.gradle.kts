428ee076b36690371630fadc1345b534e3293dbd
ForteScarlet <ForteScarlet@163.com>
2024-03-21 19:47:51 +0800

    build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    //kotlin("jvm") version "1.9.22"
 }
 
 group = "love.forte.simbot.component"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
-}
+// repositories {
+//     mavenCentral()
+// }
+//
+// dependencies {
+//     testImplementation("org.jetbrains.kotlin:kotlin-test")
+// }
+//
+// tasks.test {
+//     useJUnitPlatform()
+// }
+// kotlin {
+//     jvmToolchain(21)
+// }
