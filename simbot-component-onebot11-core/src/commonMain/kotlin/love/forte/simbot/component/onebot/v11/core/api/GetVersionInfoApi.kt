90c54ccfca5daebea1b3233a38e5aa42da2066cf
ForteScarlet <ForteScarlet@163.com>
2024-05-29 21:51:06 +0800

    迁移API: GetVersionInfoApi`

--- simbot-component-onebot11-core/src/commonMain/kotlin/love/forte/simbot/component/onebot/v11/core/api/GetVersionInfoApi.kt
+++ simbot-component-onebot11-core/src/commonMain/kotlin/love/forte/simbot/component/onebot/v11/core/api/GetVersionInfoApi.kt
@@ -1,13 +1,11 @@
 package love.forte.simbot.component.onebot.v11.core.api
 
-import kotlin.Any
-import kotlin.Nothing
-import kotlin.String
-import kotlin.jvm.JvmStatic
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import love.forte.simbot.component.onebot.v11.common.api.ApiResultConstructor
+import kotlin.jvm.JvmStatic
 
 /**
  * [`get_version_info`-获取版本信息](https://github.com/botuniverse/onebot-11/blob/master/api/public.md#get_version_info-获取版本信息)
@@ -22,7 +20,7 @@ public class GetVersionInfoApi private constructor() : OneBotApi<GetVersionInfoR
         get() = GetVersionInfoResult.serializer()
 
     override val apiResultDeserializer:
-            DeserializationStrategy<OneBotApiResult<GetVersionInfoResult>>
+        DeserializationStrategy<OneBotApiResult<GetVersionInfoResult>>
         get() = RES_SER
 
     override val body: Any?
@@ -32,7 +30,7 @@ public class GetVersionInfoApi private constructor() : OneBotApi<GetVersionInfoR
         private const val ACTION: String = "get_version_info"
 
         private val RES_SER: KSerializer<OneBotApiResult<GetVersionInfoResult>> =
-                OneBotApiResult.serializer(GetVersionInfoResult.serializer())
+            OneBotApiResult.serializer(GetVersionInfoResult.serializer())
 
         private val INSTANCE: GetVersionInfoApi = GetVersionInfoApi()
 
@@ -47,18 +45,17 @@ public class GetVersionInfoApi private constructor() : OneBotApi<GetVersionInfoR
 /**
  * [GetVersionInfoApi] 的响应体。
  *
- * @param appName 应用标识，如 `mirai-native`
- * @param appVersion 应用版本，如 `1.2.3`
- * @param protocolVersion OneBot 标准版本，如 `v11`
- * @param …… OneBot 实现自行添加的其它内容
+ * @property appName 应用标识，如 `mirai-native`
+ * @property appVersion 应用版本，如 `1.2.3`
+ * @property protocolVersion OneBot 标准版本，如 `v11`
  */
 @Serializable
-public data class GetVersionInfoResult @ApiResultType internal constructor(
+public data class GetVersionInfoResult @ApiResultConstructor internal constructor(
     @SerialName("app_name")
     public val appName: String,
     @SerialName("app_version")
     public val appVersion: String,
     @SerialName("protocol_version")
     public val protocolVersion: String,
-    public val `……`: Nothing = TODO("……?"),
+    // public val `……`: Nothing = ("……?"),
 )
