6d1d09047c01017b9489ca6e9e6c975b00b1b17f
ForteScarlet <ForteScarlet@163.com>
2024-06-03 21:45:33 +0800

    清理不再使用的模块与目录

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,8 +22,8 @@ rootProject.name = "simbot-component-onebot"
 
 include(":internal-processors:include-component-message-elements-processor")
 include(":internal-processors:event-type-resolver-processor")
+
 include(":simbot-component-onebot11-common")
 include(":simbot-component-onebot11-event")
-// include(":simbot-component-onebot11-api")
 include(":simbot-component-onebot11-message")
 include(":simbot-component-onebot11-core")
