8d155f28e67494d7271d4023dd8245892e000315
ForteScarlet <ForteScarlet@163.com>
2024-06-16 16:49:05 +0800

    version to 0.4.1

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -41,7 +41,7 @@ object P {
         override val homepage: String get() = HOMEPAGE
 
 
-        private val baseVersion = v(0, 4, 0)
+        private val baseVersion = v(0, 4, 1)
 
         val snapshotVersion = baseVersion - Version.SNAPSHOT
         override val version = if (isSnapshot()) snapshotVersion else baseVersion
