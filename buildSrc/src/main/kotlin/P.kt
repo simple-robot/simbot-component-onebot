6038b04eba842a9e5133e5b121408068b0adaae9
ForteScarlet <ForteScarlet@163.com>
2025-03-03 18:10:15 +0800

    Release: v1.6.0

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -37,8 +37,8 @@ object P {
         override val description: String get() = DESCRIPTION
         override val homepage: String get() = HOMEPAGE
 
-        const val VERSION = "1.5.0"
-        const val NEXT_VERSION = "1.6.0"
+        const val VERSION = "1.6.0"
+        const val NEXT_VERSION = "1.6.1"
 
         override val snapshotVersion = "$NEXT_VERSION-SNAPSHOT"
         override val version = if (isSnapshot()) snapshotVersion else VERSION
